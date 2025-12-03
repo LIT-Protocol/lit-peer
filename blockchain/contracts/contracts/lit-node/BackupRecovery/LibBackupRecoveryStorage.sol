58d4713e567517fac503047e132116ee3e18ea29
Howard Tam <howard.tam.95@gmail.com>
2025-12-02 20:37:13 -0800

    Add comment

--- blockchain/contracts/contracts/lit-node/BackupRecovery/LibBackupRecoveryStorage.sol
+++ blockchain/contracts/contracts/lit-node/BackupRecovery/LibBackupRecoveryStorage.sol
@@ -35,6 +35,8 @@ library LibBackupRecoveryStorage {
     struct RecoveryKey {
         bytes pubkey;
         uint256 keyType; // see rust/lit-node/src/tss/common/curve_type.rs 1 = BLS, 2 = K256, etc.  Not doing this in an enum so we can add more keytypes in the future without redeploying.
+        // NOTE: DO NOT ADD ANYTHING TO THIS STRUCT SINCE IT IS NOT CONTAINED IN A MAPPING IN THE ROOT LEVEL STORAGE STRUCT
+        // AND MAY RESULT IN STORAGE POINTERS SHIFTING.
     }
 
     /**
